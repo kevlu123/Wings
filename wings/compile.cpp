66c1cf762b47923ca7dba083aad57402210006c8
kevlu123 <kevlu321@gmail.com>
2022-03-27 18:14:01 +1300

    compile return

---
+++ wings/compile.cpp
@@ -0,0 +1,90 @@
+#include "compile.h"
+#include <unordered_map>
+
+namespace wings {
+
+	Instruction::~Instruction() {
+		switch (type) {
+		case Instruction::Type::Operation:
+			if (data.operation) {
+				delete data.operation;
+			}
+			break;
+		case Instruction::Type::Def:
+			if (data.def) {
+				delete data.def;
+			}
+			break;
+		}
+	}
+
+	Instruction::Instruction(Instruction&& rhs) noexcept :
+		Instruction()
+	{
+		*this = std::move(rhs);
+	}
+
+	Instruction& Instruction::operator=(Instruction&& rhs) noexcept {
+		std::swap(type, rhs.type);
+		std::swap(data, rhs.data);
+		return *this;
+	}
+
+	static void CompileExpression(const Expression& expression, std::vector<Instruction>& instructions) {}
+
+	static void CompileExpressionStatement(const Statement& node, std::vector<Instruction>& instructions) {
+		CompileExpression(node.expr, instructions);
+	}
+
+	static void CompileIf(const Statement& node, std::vector<Instruction>& instructions) {}
+	static void CompileElif(const Statement& node, std::vector<Instruction>& instructions) {}
+	static void CompileElse(const Statement& node, std::vector<Instruction>& instructions) {}
+	static void CompileWhile(const Statement& node, std::vector<Instruction>& instructions) {}
+	static void CompileFor(const Statement& node, std::vector<Instruction>& instructions) {}
+	static void CompileBreak(const Statement& node, std::vector<Instruction>& instructions) {}
+	static void CompileContinue(const Statement& node, std::vector<Instruction>& instructions) {}
+
+	static void CompileReturn(const Statement& node, std::vector<Instruction>& instructions) {
+		CompileExpression(node.expr, instructions);
+
+		Instruction in;
+		in.type = Instruction::Type::Return;
+		instructions.push_back(std::move(in));
+	}
+
+	static void CompileDef(const Statement& node, std::vector<Instruction>& instructions) {}
+
+	using CompileFn = void(*)(const Statement&, std::vector<Instruction>&);
+
+	static const std::unordered_map<Statement::Type, CompileFn> COMPILE_FUNCTIONS = {
+		{ Statement::Type::Expr, CompileExpressionStatement },
+		{ Statement::Type::If, CompileIf },
+		{ Statement::Type::Elif, CompileElif },
+		{ Statement::Type::Else, CompileElse },
+		{ Statement::Type::While, CompileWhile },
+		{ Statement::Type::For, CompileFor },
+		{ Statement::Type::Break, CompileBreak },
+		{ Statement::Type::Continue, CompileContinue },
+		{ Statement::Type::Return, CompileReturn },
+		{ Statement::Type::Def, CompileDef },
+	};
+
+	static void CompileStatement(const Statement& node, std::vector<Instruction>& instructions) {
+		if (COMPILE_FUNCTIONS.contains(node.type)) {
+			COMPILE_FUNCTIONS.at(node.type)(node, instructions);
+		}
+	}
+
+	static void CompileBody(const Statement& node, std::vector<Instruction>& instructions) {
+		for (const auto& child : node.body) {
+			CompileStatement(child, instructions);
+		}
+	}
+
+	std::vector<Instruction> Compile(const Statement& parseTree) {
+		std::vector<Instruction> instructions;
+		CompileBody(parseTree, instructions);
+		return instructions;
+	}
+
+}
---
+++ wings/compile.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "parse.h"
+#include "wings.h"
+#include <vector>
+
+namespace wings {
+
+	struct OperationInstructionInfo {
+		Operation op;
+		Token token; // Holds literal, variable, and/or source location of operation
+	};
+
+	struct DefInstructionInfo {
+		std::vector<Parameter> parameters;
+		std::vector<std::string> globalCaptures;
+		std::vector<std::string> localCaptures;
+		std::vector<std::string> variables;
+	};
+
+	struct Instruction {
+		enum class Type {
+			Operation,
+			Jump,
+			JumpIfTrue,
+			Def,
+			Return,
+		} type{};
+
+		union {
+			OperationInstructionInfo* operation = nullptr;
+			DefInstructionInfo* def;
+			struct {
+				// 1 less than the location to execute next
+				size_t location;
+			} jump;
+		} data;
+
+		Instruction() = default;
+		Instruction(const Instruction&) = delete;
+		Instruction(Instruction&&) noexcept;
+		Instruction& operator=(const Instruction&) = delete;
+		Instruction& operator=(Instruction&&) noexcept;
+		~Instruction();
+	};
+
+	std::vector<Instruction> Compile(const Statement& parseTree);
+}
